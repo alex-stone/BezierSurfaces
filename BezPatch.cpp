28d7f972100c2706589acd50536b3dc6f12ab729
Alex Stone <alexstone@berkeley.edu>
2013-04-04 13:47:02 -0700

    Factored out BezPatch Class

---
+++ BezPatch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "BezPatch.h"
+#include "Point.h"
+
+//****************************************************
+// BezPatch Class - Constructors
+//****************************************************
+BezPatch::BezPatch() {
+    Point tempPoint;    
+
+    for(int i = 0; i < 4; i++) {
+	for(int j = 0; j < 4; j++) {
+	    tempPoint = new Point();
+	    controlPts[i][j] = tempPoint; 
+	}
+    }
+}
+
+//****************************************************
+// BezPatch Class - Functions
+//****************************************************
+void BezPatch::print() {
+    for(int i = 0; i < 4; i++) {
+	for(int j = 0; j < 4; j++) {
+	   controlPts[i][j].printInline();
+	}
+	std::cout << std::endl;
+    }
+
+}
+
+
---
+++ BezPatch.h
@@ -0,0 +1,20 @@
+#ifndef BEZPATCH_H
+#define BEZPATCH_H
+
+#include "Point.h"
+
+//****************************************************
+// BezPatch Header Definition
+//****************************************************
+class BezPatch {
+  public:
+    // Variables
+    Point** controlPts;
+
+    // Constructors
+    BezPatch();		// Initialize all Control Points to (0,0,0)
+
+    // Functions
+    void print();
+}
+#endif
