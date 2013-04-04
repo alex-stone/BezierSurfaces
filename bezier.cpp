ac6213281cf4f5fe742183272e34cfe26c9ee6a0
Alex Stone <alexstone@berkeley.edu>
2013-04-04 13:21:43 -0700

    Prior to factoring out classes

--- bezier.cpp
+++ bezier.cpp
@@ -44,7 +44,7 @@ class Point {
 
 class BezPatch {
   public: 
-    Point controlPts[4][4];
+    Point *controlPts[4][4];
 };
 
 
@@ -52,10 +52,10 @@ class BezPatch {
 // Class Functionality
 //****************************************************
 
-Point Point(float a, float b, float c) {
-    x = a;
-    y = b;
-    z = c;
+Point::Point(float a, float b, float c) {
+    this->x = a;
+    this->y = b;
+    this->z = c;
 }
 
 
@@ -65,23 +65,23 @@ Point Point(float a, float b, float c) {
 Viewport 	viewport;
 int		numBezPatches;
 BezPatch*	bezPatches;
-char*		inputFile;
 float		subdivParam;		// Step Size in U & V for uniform, OR Error measure for adaptive
 bool		uniform;		// If true, uniform subdivision, else adaptive subdivision.
+bool		debug;
 
 //****************************************************
 // loadBezPatches Function
-//	- First line is # of Patches
-//	- Reads that many patches
+//	- First line is # of Patches (numBezPatches)
+//	- Reads that many patches into bezPatches
 //****************************************************
 void initBezPatches(char* input) {
-    ifstream inpfile(input, ifstream::in);	// Allows for input operations to be called on inpfile
+    std::ifstream inpfile(input, ifstream::in);	// Allows for input operations to be called on inpfile
 
     // Check if file is good, then check # of patches.
     if(inpfile.good()) {
 
 	// First line is number of patches
-	inpFile >> numBezPatches;
+	inpfile >> numBezPatches;
 		
 	// Create an Array of BezPatches
 	bezPatches = new BezPatch[numBezPatches];	
@@ -89,7 +89,7 @@ void initBezPatches(char* input) {
 	// Iterate through the Patches
 	for(int numPatch = 0; numPatch < numBezPatches; numPatch++) {
 
-	    BezPatch patch = new BezPatch[numBezPatches];
+	    BezPatch *patch = new BezPatch[numBezPatches];
 
 	    // Iterate through each Control Point in the Patch
 	    for(int i = 0; i < 4; i++) {
@@ -99,10 +99,10 @@ void initBezPatches(char* input) {
 		    inpfile >> y;
 		    inpfile >> z; 
 		    
-		    Point controlPt = new Point(x,y,z);
+		    Point *controlPt = new Point(x,y,z);
 		
 		    // Add current Control Point into current Patch
-		    patch.controlPts[i][j] = controlPt;
+		    patch->controlPts[i][j] = controlPt;
 		}	
 	    }
 	    // Add Patch to array of Bez Patches
@@ -112,28 +112,69 @@ void initBezPatches(char* input) {
     inpfile.close();
 }
 
+
+
+//****************************************************
+// Test Functions
+//****************************************************
+void testCommandArguments(char* inputName) {
+    std::cout << "Input File Name: " << inputName << std::endl;
+    std::cout << "Subdivision Type = ";
+    if(uniform) {
+	std::cout << "UNIFORM" << std::endl;
+    } else {
+	std::cout << "ADAPTIVE" << std:endl;
+    }
+
+}
+
 //****************************************************
 // Main Function
 //****************************************************
 int main(int argc, char *argv[]) {
 
-    // Read in the Command Line Arguments
+    char* inputFile;
+    debug = false;
+
+    // Read in the Command Line Arguments, and initialize inputFile, subdivParam, and uniform boolean.
     // If # of arguments is less than 3 it is missing either the input file name, the subdivision parameter or both.
-    if(argc < 3 || argc > 4) {
+    if(argc < 3 || argc > 5) {
 	std::cerr << "Incorrect Number of Command Line Arguments: Requires an Input File Name & Subdivision Parameter, with optional flag -a" << std::endl;
     } else {
 	inputFile = argv[1];
 	subdivParam = arv[2];
-   	if (argc == 4) {
-	    uniform = false;
-	} else {
+	if (argc == 3) {
 	    uniform = true;
+	} else {
+	    if(argc == 5) {
+		if(argv[4] == "-a" && argv[5] == "-debug") {
+		    uniform = false;
+		    debug = true;
+		} else {
+		    std::cerr << "Malformed Input" << std::endl;
+		} 
+	    } else {
+		if(argv[4] == "-a") {
+		    uniform = true;
+		} else {
+		    if(argv[4] == "-debug") {
+			debug = true;
+			uniform = false;
+		    } else {
+			std::cerr << "Malformed Input" << std::endl;
+		    }
+		}
+	    }
 	}
+    }
+
+    if(debug) {
+	testCommandArguments(inputFile);
     }
 
 
     // Initialize the Bez Patches from Input File
-    initBezPatches();
+    initBezPatches(inputFile);
 
 }
 
