8356ee5faedaeeae183c5ebf33b527a99e2640c7
Alex Stone <alexstone@berkeley.edu>
2013-04-04 21:53:41 -0700

    Added bezCurveInterp and bezPatchInterp

--- bezier.cpp
+++ bezier.cpp
@@ -115,6 +115,110 @@ void keyPress(unsigned char key, int x, int y) {
 }
 
 
+//****************************************************
+// BezCurveInterp Function: 
+//   - Given Control Points of a Bezier Curve, and a
+//        parametric value, return the curve point &
+//        derivative
+//   - Used pseudo-code from Professor O'Brien's 
+//        Slides:  Spring 2013 Lecture 12 - Surfaces
+//
+//****************************************************
+pair<Point,Point> bezCurveInterp(Point* curve, float u) {
+    
+    // The Return Value - Curve Point & Derivative
+    std::pair <Point,Point> pointDerivative;    
+
+    // Split each of the 3 segments to form two segments AB & AC
+    Point segmentA = curve[0].scalarMultiply(1.0 - u).add(curve[1].scalarMultiply(u));
+    Point segmentB = curve[1].scalarMultiply(1.0 - u).add(curve[2].scalarMultiply(u));
+    Point segmentC = curve[2].scalarMultiply(1.0 - u).add(curve[3].scalarMultiply(u));
+
+    // Split AB and BC to form new segment DE
+    Point segmentD = segmentA.scalarMultiply(1.0 - u).add(segmentB.scalarMultiply(u));
+    Point segmentE = segmentB.scalarMultiply(1.0 - u).add(segmentC.scalarMultiply(u));
+
+    // Pick the right point on DE, the point on the curve
+    pointDerivative.first = segmentD.scalarMultiply(1.0 - u).add(segmentE.scalarMultiply(u));
+
+    // Compute Derivative: (E - D) * 3
+    pointDerivative.second = segmentE.add(segmentD.scalarMultiply(-1.0)).scalarMultiply(3);
+
+    return pointDerivative;
+}
+
+
+//****************************************************
+// Helper Functions for BezPatchInterp
+//      Return the Array of 4 points, for the 
+//      appropriate V & U Curves
+//****************************************************
+
+Point* getVcurvePoints(BezPatch patch, int n) {
+    Point* curve[4];
+ 
+    curve[0] = patch.controlPoints[n][0];
+    curve[1] = patch.controlPoints[n][1];
+    curve[2] = patch.controlPoints[n][2];
+    curve[3] = patch.controlPoints[n][3];
+
+    return curve;
+}
+
+Point* getUcurvePoints(BezPatch patch, int n) {
+    Point* curve[4];
+
+    curve[0] = patch.controlPoints[0][n];
+    curve[1] = patch.controlPoints[1][n];
+    curve[2] = patch.controlPoints[2][n];
+    curve[3] = patch.controlPoints[3][n];
+    
+    return curve;
+}
+
+
+//****************************************************
+// BezPatchInterp Function:
+//   - Given a Control Patch and (u,v) values, find
+//      the surface point and normal
+//****************************************************
+pair<Point,Point> bezPatchInterp(BezPatch patch, float u, float v) {
+    Point vcurve[4];
+    Point ucurve[4];
+
+    std::pair <Point,Point> pointNormal;
+    std::pair <Point,Point> VptDeriv;
+    std::pair <Point,Point> UptDeriv;
+
+    // Build Control Points for Bezier Curve in V
+    vcurve[0] = bezCurveInterp(getVcurvePoints(patch, 0), u).first;
+    vcurve[1] = bezCurveInterp(getVcurvePoints(patch, 1), u).first;
+    vcurve[2] = bezCurveInterp(getVcurvePoints(patch, 2), u).first;
+    vcurve[3] = bezCurveInterp(getVcurvePoints(patch, 3), u).first;
+
+    // Build Control Points for Bezier Curve in U
+    ucurve[0] = bezCurveInterp(getUcurvePoints(patch, 0), v).first;
+    ucurve[1] = bezCurveInterp(getUcurvePoints(patch, 1), v).first;
+    ucurve[2] = bezCurveInterp(getUcurvePoints(patch, 2), v).first;
+    ucurve[3] = bezCurveInterp(getUcurvePoints(patch, 3), v).first;
+
+
+    // Evaluate Surface & Derivative for U & V
+    VptDeriv = bezCurveInterp(vcurve, v);
+    UptDeriv = bezCurveInterp(ucurve, u);
+
+    Point dPdv = VptDeriv.second;
+    Point dPdu = UptDeriv.second;
+ 
+    // Take Cross Product of partials to find Normal:
+    Point normal = dPdu.cross(dPdv);
+    normal.normalize();
+
+    pointNormal.first = VptDeriv.first;
+    pointNormal.second = normal;
+
+    return pointNormal;
+}
 
 
 //****************************************************
